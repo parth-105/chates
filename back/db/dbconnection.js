7c3cd80a8dcbb3db4cbf1fe8751c98452a19c5aa
parth <kalathiyaharsh7@gmail.com>
2024-06-06 18:04:53 +0530

    first commit

--- back/db/dbconnection.js
+++ back/db/dbconnection.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose')
 const connectiondb = async() =>{
 
     try{
-        await mongoose.connect('mongodb://127.0.0.1:27017/chat');
+        // await mongoose.connect('mongodb://127.0.0.1:27017/chat');
+        await mongoose.connect(`mongodb+srv://parth:knXIfzOnNqAvMduh@cluster0.4roqkjc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`);
         console.log("connect to database");
     }
 
