b6771ba2151f3f047b69ca48af4dd83fbc81f42b
parth-105 <kalathiyaharsh7@gmail.com>
2024-10-10 08:45:03 +0530

    Update

--- back/db/dbconnection.js
+++ back/db/dbconnection.js
@@ -4,7 +4,7 @@ const connectiondb = async() =>{
 
     try{
         // await mongoose.connect('mongodb://127.0.0.1:27017/chat');
-        await mongoose.connect(`mongodb+srv://parth:knXIfzOnNqAvMduh@cluster0.4roqkjc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`);
+       
         console.log("connect to database");
     }
 
